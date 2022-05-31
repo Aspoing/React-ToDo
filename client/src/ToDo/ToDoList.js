aeec79ed4335e4f29da195b0b0207f6a107e1810
Sandro SGRO <sandro.sgro@epitech.eu>
2022-05-31 15:48:32 +0200

    [MOD] problems with server

---
+++ client/src/ToDo/ToDoList.js
@@ -0,0 +1,59 @@
+import React, {Component} from "react"
+
+class ToDoList extends Component {
+    constructor() {
+        super();
+        this.state = {
+            userInput: '',
+            items: []
+        }
+    }
+
+    onChange(event) {
+        this.setState({ userInput: event.target.value })
+    }
+
+    addTodo(event) {
+        event.preventDefault();
+        this.setState({ items: [...this.state.items, this.state.userInput] }, () => console.log(this.state))
+    }
+
+    deleteToDo(event) {
+        event.preventDefault();
+        const array = this.state.items;
+        const index = array.indexOf(event.target.value);
+        array.splice(index, 1);
+        // array.splice(0, 0, item);
+        this.setState({ items: array });
+    }
+
+    renderToDo() {
+        return this.state.items.map((item) => {
+            return (
+                <div key={item}>
+                    {console.log(item)}
+                    {item} | <button onClick={this.deleteToDo.bind(this)}>X</button>
+                </div>
+            );
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <h1>ToDo List</h1>
+                <form>
+                    <input value={this.state.userInput} type="text" placeholder="Renseigner un item"
+                        onChange={this.onChange.bind(this)}
+                    />
+                    <button onClick={this.addTodo.bind(this)}>Ajouter un item</button>
+                </form>
+                <div>
+                    {this.renderToDo()}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default ToDoList;
